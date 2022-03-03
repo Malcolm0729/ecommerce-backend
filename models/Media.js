d1388b22368eb7bcfa8019513db690647a54139e
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-03-02 23:44:00 -0800

    initial commit for media js

---
+++ models/Media.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Media extends Model {}
+
+Media.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [['sports', 'movie', 'video game']],
+      },
+    },
+    info: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'media',
+  }
+);
+
+module.exports = Media;
