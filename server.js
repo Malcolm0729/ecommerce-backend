f9c1d66858145ac3a0e8f7183cf30a5a8b3435af
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-18 19:17:45 -0700

    Final changes to ecommerce

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-// const routes = require('./routes');
 
 // import sequelize connection
 const sequelize = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
