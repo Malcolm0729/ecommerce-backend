ba37592cdddf5a3c1bf5c0b9558d2f175b931288
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-18 17:24:10 -0700

    fixing connection js file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
 
 // app.listen(PORT, () => {
